f81ff311fd826a0fe2df824a9e222bd66c0e2e71
David Wiggins <davidchriswiggins@gmail.com>
2023-03-06 17:17:44 +1100

    Hurtbox system foundations in place. Overlap circles return list of all colliders hit, we'll push this list to a queue and then dequeue pushing the collider list to all listeners.

--- Assets/Collision/HurtboxManager.cs
+++ Assets/Collision/HurtboxManager.cs
@@ -11,6 +11,8 @@ public class HurtboxManager : MonoBehaviour
     List<FrameCollisionData> hurtboxes = new List<FrameCollisionData>();
 
     int frame;
+
+    public List<Collider2D> lastCastReturn = new List<Collider2D>();
     void Start()
     {
         animator = GetComponentInChildren<Animator>();
@@ -29,8 +31,6 @@ public class HurtboxManager : MonoBehaviour
             hurtboxes = collisionManager.GetHurtboxData(currentClipInfo.clip.name);
         }
     }
-
-    // Update is called once per frame
     void Update()
     {
         if(currentClipInfo.clip != null)
@@ -46,6 +46,37 @@ public class HurtboxManager : MonoBehaviour
         currentClipInfo = animator.GetCurrentAnimatorClipInfo(0)[0];
 
         frame = animator.CurrentFrame();
+
+        CastHurtboxes();
+    }
+
+    void CastHurtboxes()
+    {
+        if (hurtboxes == null)
+            return;
+
+        if (hurtboxes.Count > 0 && hurtboxes[frame].circles.Count > 0)
+        {
+            List<Collider2D> collidersHit = new List<Collider2D>();
+            for (int i = 0; i < hurtboxes[frame].circles.Count; i++)
+            {
+                Vector3 circlePos = transform.position + new Vector3(
+                        hurtboxes[frame].circles[i].center.x * animator.transform.localScale.x,
+                        hurtboxes[frame].circles[i].center.y,
+                        0f);
+
+                float circleRadius = hurtboxes[frame].circles[i].radius;
+
+                Collider2D[] colliders = Physics2D.OverlapCircleAll(circlePos, circleRadius);
+
+                foreach (Collider2D collider in colliders)
+                {
+                    collidersHit.Add(collider);
+                }
+            }
+
+            lastCastReturn = collidersHit;
+        }
     }
 
     public bool showHurtboxGizmos = false;
