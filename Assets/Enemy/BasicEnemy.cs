69de92a6065d6621159e94c5a9c70f4858a56721
David Wiggins <davidchriswiggins@gmail.com>
2023-03-17 12:57:52 +1100

    Fixed stuff so we don't have a weird problem with negative damage recovery time

--- Assets/Enemy/BasicEnemy.cs
+++ Assets/Enemy/BasicEnemy.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class BasicEnemy : MonoBehaviour, Listener_DamageReceiver
 {
     int damageCount;
-    float damageRecoverTime;
+    public float damageRecoverTime;
     float timer;
     HurtboxOverlap lastOverlap;
     void Start()
@@ -27,7 +27,7 @@ public class BasicEnemy : MonoBehaviour, Listener_DamageReceiver
         //Compute how many seconds left until the current animation state has finished
         //This will result in the first overlap instance in this animation being able to deal damage but no others
         //Hopefully this will stop damage from going through multiple times during one damage window
-        damageRecoverTime = overlap.stateLength - (overlap.stateNormalizedTime * overlap.clip.length + 1) / overlap.stateSpeed;
+        damageRecoverTime = overlap.stateLength - (overlap.stateNormalizedTime * overlap.clip.length) / overlap.stateSpeed;
 
         if (damageRecoverTime < 0)
             Debug.LogError($"Damage Recover Time is negative: This is not fucking good, please increase frames of animation that is causing this. Anim Clip Name = {overlap.clip.name}");
