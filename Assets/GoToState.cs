dce0619ee96c382e9e92f57f600e9eda9cecd3b7
David Wiggins <davidchriswiggins@gmail.com>
2023-03-23 00:13:27 +1100

    Hammer ground attacks fully implemented

--- Assets/GoToState.cs
+++ Assets/GoToState.cs
@@ -5,7 +5,6 @@ public class GoToState : StateMachineBehaviour
 {
     //This must name a state properly
     public string stateName;
-    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
         animator.Play(stateName);
